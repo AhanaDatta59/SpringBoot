dfa0a087e2d7bfe78e6bf1a49f2979cfe5be2517
AhanaDatta59 <ahanadatta59@gmail.com>
2024-01-13 17:39:20 +0530

    Controller Initialization

--- src/main/java/com/springboot/SpringBoot/controller/HelloController.java
+++ src/main/java/com/springboot/SpringBoot/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.springboot.SpringBoot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome here Ahnaaaa!!";
+    }
 }
