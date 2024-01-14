c2c9e8a0b35f88d043b60a216538c67b758f62fc
AhanaDatta59 <ahanadatta59@gmail.com>
2024-01-14 10:51:40 +0530

    Get and GetByID implemented

--- src/main/java/com/springboot/SpringBoot/controller/DepartmentController.java
+++ src/main/java/com/springboot/SpringBoot/controller/DepartmentController.java
@@ -1,12 +1,10 @@
 package com.springboot.SpringBoot.controller;
-
+import java.util.List;
 import com.springboot.SpringBoot.entity.Department;
 import com.springboot.SpringBoot.service.DepartmentService;
 import com.springboot.SpringBoot.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepartmentController {
@@ -18,5 +16,15 @@ public class DepartmentController {
     public Department saveDepartment(@RequestBody Department department){
 //        DepartmentService service = new DepartmentServiceImpl();
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
     }
 }
--- src/main/java/com/springboot/SpringBoot/service/DepartmentService.java
+++ src/main/java/com/springboot/SpringBoot/service/DepartmentService.java
@@ -2,6 +2,12 @@ package com.springboot.SpringBoot.service;
 
 import com.springboot.SpringBoot.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+     public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
 }
